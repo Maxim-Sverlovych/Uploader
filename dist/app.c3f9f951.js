16be2908775a391618576b1875695c5ed7b5c484
Maxim-Sverlovych <msverlovych@gmail.com>
2021-02-08 16:33:30 +0200

    add read.md file

--- dist/app.c3f9f951.js
+++ dist/app.c3f9f951.js
@@ -8793,7 +8793,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63767" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51297" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Uploader
+
+### Веб плагин для закгрузки фотографий на сервер
+Допустимые форматы файлов: png, jpg, jpeg, svg. 
+
+В качестве серверного хранилища, я использую *firebase-storage*.
+
+---
+
+### Web plugin for uploading photos to the server
+
+Valid file format: png, jpg, jpeg, svg. 
+
+I using *firebase - storage* as server storage.
